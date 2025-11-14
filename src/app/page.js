def873448297f78e0ce5b96d0263516f09329ef1
25LP8703 25LP8703 <25LP8703@pinecones-iMac.local>
2025-11-14 11:27:05 +0800

    finished

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,79 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const cells = ["", "", "", "", "", "", "", "", ""];
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+  const [turn, setTurn] = useState("X");
+  const [winner, setWinner] = useState(null);
 
-  const [text, setText] = useState("playerO");
+  const handleclick = (index) => () => {
+    const newBoard = [...board];
+    if (newBoard[index] !== "") return;
+    newBoard[index] = turn;
+    setBoard(newBoard);
+    setTurn(turn === "X" ? "O" : "X");
+    checkWinner();
+  };
+
+  useEffect(() => {
+    const checkWinner = () => {
+      if (board[0] === board[1] && board[1] === board[2] && board[0] !== "") {
+        setWinner(board[0]);
+      } else if (
+        board[3] === board[4] &&
+        board[4] === board[5] &&
+        board[3] !== ""
+      ) {
+        setWinner(board[3]);
+      } else if (
+        board[6] === board[7] &&
+        board[7] === board[8] &&
+        board[6] !== ""
+      ) {
+        setWinner(board[6]);
+      } else if (
+        board[0] === board[3] &&
+        board[3] === board[6] &&
+        board[0] !== ""
+      ) {
+        setWinner(board[0]);
+      } else if (
+        board[1] === board[4] &&
+        board[4] === board[7] &&
+        board[1] !== ""
+      ) {
+        setWinner(board[1]);
+      } else if (
+        board[2] === board[5] &&
+        board[5] === board[8] &&
+        board[2] !== ""
+      ) {
+        setWinner(board[2]);
+      } else if (
+        board[0] === board[4] &&
+        board[4] === board[8] &&
+        board[0] !== ""
+      ) {
+        setWinner(board[0]);
+      } else if (
+        board[2] === board[4] &&
+        board[4] === board[6] &&
+        board[2] !== ""
+      ) {
+        setWinner(board[2]);
+      }
+    };
+    checkWinner();
+  }, [board]);
+
+  if (winner) {
+    return (
+      <div className="text-[24px] flex justify-center text-black">
+        {winner} hojlee
+      </div>
+    );
+  }
 
   return (
     <div className="flex justify-center flex-col">
@@ -13,12 +81,15 @@ export default function Home() {
         Tic Tac Toe
       </p>
       <div className="w-[680px] h-[680px] grid grid-cols-3 grid-rows-3 gap-5 border-4 border-black self-center p-5 text-black">
-        {cells.map((cell, index) => {
+        {board.map((item, index) => {
           return (
             <div
               key={index}
-              className="w-[200px] h-[200px] border-2 border-black "
-            ></div>
+              className="w-[200px] h-[200px] border-2 border-black text-[72px] flex justify-center items-center"
+              onClick={handleclick(index)}
+            >
+              {item}
+            </div>
           );
         })}
       </div>
